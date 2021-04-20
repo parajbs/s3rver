d6f560bc46e468dd416b85bda469bcec344d6ce2
parajbs <parajbs@gmx.net>
2021-04-20 22:45:08 +0200

    add  webhosting-plesk-nodejs-setup

---
+++ plesk_app.js
@@ -0,0 +1,41 @@
+const os = require('os');
+const fs = require('fs');
+const S3rver = require('./lib/s3rver');
+const { fromEvent } = require('rxjs');
+const { filter } = require('rxjs/operators');
+
+const corsConfig = require.resolve('./example/cors.xml');
+const websiteConfig = require.resolve('./example/website.xml');
+
+const instance = new S3rver({
+  port: 80,
+  address: 'S3domain',
+  silent: false,
+  logfile: false,
+  serviceEndpoint: 'S3domain',
+  directory: './tmp/s3rver',
+  resetOnClose: false,
+  allowMismatchedSignatures: true,
+  vhostBuckets: false,
+  configureBuckets: [
+    {
+      name: 'conf_web',
+      configs: [fs.readFileSync(corsConfig), fs.readFileSync(websiteConfig)],
+    },
+    {
+      name: 'test-bucket',
+    },
+  ],
+}).run((err, { address, port } = {}) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('now listening at address %s and port %d', address, port);
+  }
+});
+
+const s3Events = fromEvent(instance, 'event');
+s3Events.subscribe((event) => console.log(event));
+s3Events
+  .pipe(filter((event) => event.Records[0].eventName == 'ObjectCreated:Copy'))
+  .subscribe((event) => console.log(event));
